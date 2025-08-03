66e6448421becb1db1789070a5b8b5e1b78a040e
AbdElRahman Saad <102773742+AbdElRahman-Saad-ElDin@users.noreply.github.com>
2025-08-03 06:37:40 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "npx-tagger";
 
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
+  plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/Restaurant-App",
   resolve: {
     alias: {
